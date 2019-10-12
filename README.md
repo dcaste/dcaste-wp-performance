f4a4719460d9d923cce270194e7501fe23213040
unknown <daxcastellon@gmail.com>
2019-10-12 22:07:13 +0200

    Translate snippets and adde how to use in readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # dcaste-wp-performance
-A small plugin that adds performance improvements to WordPress by removing and tweaking WP options.
-Does not add calls to the database, instead all of the tweaks are made in PHP, just enable or disable those you need.
+
+## What is this?
+A small collection of snippets that improves WordPress performance by removing or tweaking some options.
+
+These improvements are tailored for developers since all of the tweaks are made directly in a PHP file, so if you want to use them just enable or disable those you need by commenting or deleting them as you need it.
+
+### Why this approach?
+Sometimes as a developer you need to improve WordPress performance and don't want to add a lot of plugins or bloat your database for simple tweaks. 
+
+## How to use as a plugin:
+- Upload the folder to the WordPress plugin folder.
+- Enable or disable those tweaks you need by commenting or deleting them. All tweaks has comments on how to use them.
+- Activate the plugin.
+
+## How to use as snippets in yout functions.php.
+- Copy the snippets you need in your theme's functions.php file.
--- dcaste-wp-performance.php
+++ dcaste-wp-performance.php
@@ -12,68 +12,75 @@ defined( 'ABSPATH' ) || exit;
 $url = plugin_dir_url( __FILE__ );
 
 
-/* REST API *******************************************************************/
 
-// Deshabilita el sistema de REST API completamente.
-// Utilizar con precaución pues aLgunos plugins pueden depender de esta funcionalidad.
-add_filter( 'rest_authentication_errors', 'deshabilitar_rest_api' );
-function deshabilitar_rest_api( $access ) {
-	return new WP_Error( 'rest_disabled', 'El REST API en este sitio ha sido deshabilitado', array( 'status' => rest_authorization_required_code() ) );
+/**
+ * REST API improvements.
+ * Be careful with these tweaks, they can break your site since most plugins make calls to the REST API.
+ * 
+ */
+
+// Disables REST API completely.
+add_filter( 'rest_authentication_errors', 'dcaste_disable_rest_api' );
+function dcaste_disable_rest_api( $access ) {
+	return new WP_Error( 'rest_disabled', 'REST API is disabled', array( 'status' => rest_authorization_required_code() ) );
 }
 
-// Inhabilita el acceso al REST API si no es un usuario conectado.
+// Disables REST API if is not an authenticated user.
 add_filter( 'rest_authentication_errors', function( $result ) {
 	if ( ! empty( $result ) ) {
 		return $result;
 	}
 	if ( ! is_user_logged_in() ) {
-		return new WP_Error( 'rest_not_logged_in', 'No está conectado.', array( 'status' => 401 ) );
+		return new WP_Error( 'rest_not_logged_in', 'You are not logged in.', array( 'status' => 401 ) );
 	}
 	return $result;
 });
 
-// Deshabilita el enlace de REST API en el head.
+// Disables REST API in head.
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 
-// Deshabilita enlaces de oEmbeds en el head.
+// Disables oEmbeds in head.
 remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
 
-// Deshabilita el enlace a REST API en los HTTP headers.
+// Disables REST API links in HTTP headers.
 remove_action('template_redirect', 'rest_output_link_header', 11, 0);
 
-// Deshabilita la carga de JSON del REST API.
+// Disables REST API JSON.
 add_filter('json_enabled', '__return_false');
 add_filter('json_jsonp_enabled', '__return_false');
 
 
 
-/* HEARTBEAT ******************************************************************/
-
-// Deshabilita completamente la funcionalidad de heartbeat.
-add_action( 'init', 'apaga_heartbeat', 1 );
-function apaga_heartbeat() {
+/**
+ * WordPress Heartbeat Improvements.
+ * Be careful because Gutenberg editor uses the heartbeat functionality to refresh content.
+ * 
+ */
+// Disables WordPress heartbeat completely.
+add_action( 'init', 'dcaste_turn_off_heartbeat', 1 );
+function dcaste_turn_off_heartbeat() {
 	wp_deregister_script('heartbeat');
 }
 
-// Deshabilita heartbeat en homepage solamente.
-add_action( 'init', 'apaga_heartbeat_home', 1 );
-function apaga_heartbeat_home() {
+// Disables heartbeat in home only.
+add_action( 'init', 'dcaste_turn_off_heartbeat_home', 1 );
+function dcaste_turn_off_heartbeat_home() {
 	global $pagenow;
 	if ( $pagenow == 'index.php' ){
 		wp_deregister_script( 'heartbeat' );
 	}
 }
 
-// Deshabilita heartbeat en posts y páginas solamente.
-add_action( 'init', 'apaga_heartbeat_posts', 1 );
-function apaga_heartbeat_posts() {
+// Disables heartbeat in posts and pages only.
+add_action( 'init', 'dcaste_turn_off_heartbeat_posts', 1 );
+function dcaste_turn_off_heartbeat_posts() {
 	global $pagenow;
 	if ( $pagenow != 'post.php' && $pagenow != 'post-new.php' ){
 		wp_deregister_script('heartbeat');
 	}
 }
 
-// Modifica la frecuencia del heartbeat.
+// Modifies heartbeat frequency.
 $heartbeat_location  = get_option('heartbeat_location');
 $heartbeat_frequency = get_option('heartbeat_frequency');
 
@@ -90,11 +97,14 @@ if ( is_numeric( $heartbeat_frequency ) ) {
 }
 
 
-/* WIDGETS ********************************************************************/
+/**
+ * Widgets improvements.
+ * 
+ */
 
-//Desactiva todos los widgets que trae consigo WP por defecto. Comentar los que no se desean desactivar.
-add_action( 'widgets_init', 'remover_widgets');
-function remover_widgets() {
+// Disables default widgets. Comment out those you need to use. 
+add_action( 'widgets_init', 'dcaste_disable_widgets');
+function dcaste_disable_widgets() {
 	unregister_widget('WP_Widget_Pages');
 	unregister_widget('WP_Widget_Calendar');
 	unregister_widget('WP_Widget_Archives');
@@ -117,36 +127,33 @@ function remover_widgets() {
 
 
 
-/* RSS ************************************************************************/
+/**
+ * RSS improvements.
+ * 
+ */
 
-// Deshabilita los feeds de RSS.
-add_action('do_feed', 'deshabilitar_rss', 1);
-add_action('do_feed_rdf', 'deshabilitar_rss', 1);
-add_action('do_feed_rss', 'deshabilitar_rss', 1);
-add_action('do_feed_rss2', 'deshabilitar_rss', 1);
-add_action('do_feed_atom', 'deshabilitar_rss', 1);
-add_action('do_feed_rss2_comments', 'deshabilitar_rss', 1);
-add_action('do_feed_atom_comments', 'deshabilitar_rss', 1);
-function deshabilitar_rss() {
-	wp_die( 'No existe RSS' );
+// Disables RSS Feeds.
+add_action('do_feed', 'dcaste_disable_rss', 1);
+add_action('do_feed_rdf', 'dcaste_disable_rss', 1);
+add_action('do_feed_rss', 'dcaste_disable_rss', 1);
+add_action('do_feed_rss2', 'dcaste_disable_rss', 1);
+add_action('do_feed_atom', 'dcaste_disable_rss', 1);
+add_action('do_feed_rss2_comments', 'dcaste_disable_rss', 1);
+add_action('do_feed_atom_comments', 'dcaste_disable_rss', 1);
+function dcaste_disable_rss() {
+	wp_die( 'RSS does not exists.' );
 }
 
 
 
-// Desactiva la llamada automática de fuentes de Google.
-add_filter( 'style_loader_src', function($href){
-	if(strpos($href, "//fonts.googleapis.com/") === false) {
-		return $href;
-	}
-		return false;
-});
+/**
+ * Comments improvements.
+ * 
+ */
 
-
-/* COMENTARIOS ****************************************************************/
-
-// Deshabilita comentarios y trackbacks en los posts.
-add_action('admin_init', 'deshabilita_comentarios_posts');
-function deshabilita_comentarios_posts() {
+// Disables comments and trackback in posts.
+add_action('admin_init', 'dcaste_diable_comments_posts');
+function dcaste_diable_comments_posts() {
 	$post_types = get_post_types();
 	foreach ($post_types as $post_type) {
 		if(post_type_supports($post_type, 'comments')) {
@@ -156,27 +163,38 @@ function deshabilita_comentarios_posts() {
 	}
 }
 
-// Cierra los comentarios abiertos.
-add_filter('comments_open', 'cierra_comentarios_abiertos', 20, 2);
-add_filter('pings_open', 'cierra_comentarios_abiertos', 20, 2);
-function cierra_comentarios_abiertos() {
+// Close open comments.
+add_filter('comments_open', 'dcaste_close_comments', 20, 2);
+add_filter('pings_open', 'dcaste_close_comments', 20, 2);
+function dcaste_close_comments() {
 	return false;
 }
 
-// Ocultar los comnetarios existentes.
-add_filter('comments_array', 'ocultar_comentarios', 10, 2);
-function ocultar_comentarios($comments) {
+// Hide existing comments.
+add_filter('comments_array', 'dcaste_hide_comments', 10, 2);
+function dcaste_hide_comments($comments) {
    $comments = array();
    return $comments;
 }
 
 
 
-/* LIMPIEZA DE SCRIPTS VARIOS *************************************************/
+/**
+ * Miscellaneous improvements.
+ * 
+ */
 
-// Deshabilita el script de emojis.
-add_action( 'init', 'deshabilitar_emojis' );
-function deshabilitar_emojis() {
+// Disables automatic Google Fonts calls.
+add_filter( 'style_loader_src', function($href){
+	if(strpos($href, "//fonts.googleapis.com/") === false) {
+		return $href;
+	}
+		return false;
+});
+
+// Disable emojis fonts.
+add_action( 'init', 'dcaste_disable_emojis' );
+function dcaste_disable_emojis() {
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
   remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -188,9 +206,9 @@ function deshabilitar_emojis() {
   add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
 }
 
-// Remueve JQuery migrate
-add_action('wp_default_scripts', 'remueve_jquery_migrate');
-function remueve_jquery_migrate($scripts)
+// Remove JQuery migrate.
+add_action('wp_default_scripts', 'dcaste_remove_jquery_migrate');
+function dcaste_remove_jquery_migrate($scripts)
 {
 	if (!is_admin() && isset($scripts->registered['jquery'])) {
 	  $script = $scripts->registered['jquery'];
@@ -202,41 +220,31 @@ function remueve_jquery_migrate($scripts)
   }
 }
 
-// Remueve el script wp-embed.
-add_action( 'wp_footer', 'remueve_wp_embed' );
-function remueve_wp_embed(){
+// Removes wp-embed script.
+add_action( 'wp_footer', 'dcaste_removes_wp_embed' );
+function dcaste_removes_wp_embed(){
 	wp_dequeue_script( 'wp-embed' );
 }
 
-
-
-/* VARIOS *********************************************************************/
-
-// Deshabilita el enlace  a RSD.
+// Removes RSD link.
 remove_action ('wp_head', 'rsd_link');
 
-// Deshabilita el enlace de wlw.
+// Removes wlw link.
 remove_action( 'wp_head', 'wlwmanifest_link');
 
-// Deshabilita los 'shortlinks'.
+// Disables shortlinks.
 remove_action( 'wp_head', 'wp_shortlink_wp_head');
 
-// Remueve el mensaje de generador de Wordpress.
-add_filter('the_generator', 'remueve_generador_wp');
-function remueve_generador_wp() {
+// Removes WordPress generator message.
+add_filter('the_generator', 'dcaste_removes_wp_generator');
+function dcaste_removes_wp_generator() {
 	return '';
 }
 
-// Desactiva el llamado a pings del cron de WP.
-if (isset($_GET['doing_wp_cron'])) {
-	remove_action('do_pings', 'do_all_pings');
-	wp_clear_scheduled_hook('do_pings');
-}
-
-// Remueve las versiones de los scripts.
-add_filter( 'script_loader_src', 'remueve_version_script', 15, 1 );
-add_filter( 'style_loader_src', 'remueve_version_script', 15, 1 );
-function remueve_version_script( $src ){
+// Removes script versions.
+add_filter( 'script_loader_src', 'dcaste_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'dcaste_remove_script_version', 15, 1 );
+function dcaste_remove_script_version( $src ){
 	$parts = explode( '?', $src );
 	return $parts[0];
 }
